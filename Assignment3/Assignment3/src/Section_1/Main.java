5af633c1744947d8aab034cee12d00e8ffc37f00
Benny-JH-L <bennyliang24@gmail.com>
2024-06-05 17:19:58 -0600

    changed and added some stuff to Section_1

--- Assignment3/Assignment3/src/Section_1/Main.java
+++ Assignment3/Assignment3/src/Section_1/Main.java
@@ -1,9 +1,12 @@
 package Section_1;
 
+import Section_1.DTBST.*;
+
 public class Main 
 {
     public static void main(String[] args) throws Exception 
     {
         System.out.println("Hello, World!");
+        
     }
 }
---
+++ Assignment3/Assignment3/src/Section_1/TreeNode.java
@@ -0,0 +1,17 @@
+package Section_1;
+
+// public class TreeNode
+// {
+//     Event event;
+//     TreeNode left, right;
+//     boolean leftThread, rightThread;
+
+//     public TreeNode(Event event)
+//     {
+//         this.event = event;
+//         this.left = null;
+//         this.right = null;
+//         this.leftThread = true;
+//         this.rightThread = true;
+//     }
+// }

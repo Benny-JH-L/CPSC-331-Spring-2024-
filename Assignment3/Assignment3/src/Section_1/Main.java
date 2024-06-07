969dd5c8d73fadd68e98966e949dd870df2b98b6
Benny-JH-L <bennyliang24@gmail.com>
2024-06-07 17:30:43 -0600

    added name

--- Assignment3/Assignment3/src/Section_1/Main.java
+++ Assignment3/Assignment3/src/Section_1/Main.java
@@ -2,6 +2,9 @@ package Section_1;
 
 import Section_1.DTBST.*;
 
+// CPSC 331 -Spring 2024- Assignment 3 | Advanced ADT With Applications
+// Name: Benny Liang | UCID: 30192142
+
 public class Main 
 {
     public static void main(String[] args) throws Exception 

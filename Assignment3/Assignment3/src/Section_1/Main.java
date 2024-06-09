0760bcf1bce9bc1ad4384f8e465b9d34b493c93e
Benny-JH-L <bennyliang24@gmail.com>
2024-06-09 17:20:09 -0600

    in test2() it follows and creates the same tree as in vid

--- Assignment3/Assignment3/src/Section_1/Main.java
+++ Assignment3/Assignment3/src/Section_1/Main.java
@@ -16,12 +16,23 @@ public class Main
         Event e0 = new Event("name0", 5000, 30);
         Event e1 = new Event("name1", 120, 25);
         boolean c0 = bst.checkConflict(e0, e1);  // expect to return true (conflict)
-        System.out.printf("\nDoes Event<%s> added conflict when compared to Event<%s> | %s\n\n", e1.toString(), e0.toString(), c0);
+        System.out.printf("\nDoes Event<%s> added conflict when compared to Event<%s> | %s\n\n\n", e1.toString(), e0.toString(), c0);
 
-        bst = new DTBST();
+        // addTest1();
+        addTest2();
+    }
 
+    /**
+     * My test cases, will be deleted before submition.
+     */
+    private static void addTest1()
+    {
+        System.out.println("----Add Test 1---");
+        DTBST bst = new DTBST();
+        Event e0 = new Event("name0", 5000, 30);
         boolean b0 = bst.addEvent(e0);
         System.out.printf("Added Event<%s> | %s\n\n", e0.toString(), b0);
+        Event e1 = new Event("name1", 120, 25);
         boolean b1 = bst.addEvent(e1);
         System.out.printf("Added Event<%s> | %s\n\n", e1.toString(), b1);
         Event e2 = new Event("name2", 200, 5);
@@ -33,5 +44,45 @@ public class Main
         Event e4 = new Event("Name4", 100, 15);
         boolean b4 = bst.addEvent(e4);
         System.out.printf("Added Event<%s> | %s\n\n", e4.toString(), b4);
+    }
+
+    private static void addTest2()
+    {
+        System.out.println("----Add Test 2---");
+        DTBST bst = new DTBST();
+
+        int duration = 1;
+        Event e = new Event("n20", 20, duration);
+        boolean b = bst.addEvent(e);
+        System.out.printf("Added Event<%s> | %s\n\n", e.toString(), b);
+
+        e = new Event("n10", 10, duration);
+        b = bst.addEvent(e);
+        System.out.printf("Added Event<%s> | %s\n\n", e.toString(), b);
+
+        e = new Event("n30", 30, duration);
+        b = bst.addEvent(e);
+        System.out.printf("Added Event<%s> | %s\n\n", e.toString(), b);
+
+        e = new Event("n5",5, duration);
+        b = bst.addEvent(e);
+        System.out.printf("Added Event<%s> | %s\n\n", e.toString(), b);
+
+        e = new Event("n16", 16, duration);
+        b = bst.addEvent(e);
+        System.out.printf("Added Event<%s> | %s\n\n", e.toString(), b);
+
+        e = new Event("n14", 14, duration);
+        b = bst.addEvent(e);
+        System.out.printf("Added Event<%s> | %s\n\n", e.toString(), b);
+
+        e = new Event("n17", 17, duration);
+        b = bst.addEvent(e);
+        System.out.printf("Added Event<%s> | %s\n\n", e.toString(), b);
+
+        e = new Event("n13", 13, duration);
+        b = bst.addEvent(e);
+        System.out.printf("Added Event<%s> | %s\n\n", e.toString(), b);
+
     }
 }

7ff9e32d0399c3c803601115924bca8814456279
Benny-JH-L <bennyliang24@gmail.com>
2024-06-12 17:35:52 -0600

    tutorial today

---
+++ Tutorial/June12/June12/src/QuickSort.java
@@ -0,0 +1,9 @@
+
+public class QuickSort<T extends Comparable>
+{
+    public static Student[] sort(Student[] arr)
+    {
+        
+    }
+
+}
---
+++ Tutorial/June12/June12/src/Student.java
@@ -0,0 +1,17 @@
+
+public class Student
+{
+    public String name;
+    public int GPA;
+
+    public Student(String name, int GPA)
+    {
+        this.name = name;
+        this.GPA = GPA;
+    }
+
+    // public int compareTo(Student o) 
+    // {
+
+    // }
+}
